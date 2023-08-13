b96d5c3de77f7964a0ac1eefa67f555bc17033d7
kasinath777 <74859052+kasinath777@users.noreply.github.com>
2023-08-13 14:49:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+https://teachablemachine.withgoogle.com/models/i20zHI_cw/;
+
+Webcam.set({
+width: 350,
+height: 350,
+image_format: "png",
+png_quality: 100
+})
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera")
+
+function take_snapshot(){
+Webcam.snap(function (data_uri){
+document.getElementById("result").innerHTML='<img id="pic" src="'+data_uri+'"/>'
+
+})
+}
